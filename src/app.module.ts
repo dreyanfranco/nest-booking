ad588ff4de8ce3d9a75663aa2674995a3b22781e
Dreyan Franco <dreyanfranco@gmail.com>
2024-08-25 22:46:06 +0200

    user schema, db created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { SearchModule } from './search/search.module';
-import { HotelModule } from './hotel/hotel.module';
 import { BookingModule } from './booking/booking.module';
+import { HotelModule } from './hotel/hotel.module';
+import { SearchModule } from './search/search.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, SearchModule, HotelModule, BookingModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    SearchModule,
+    HotelModule,
+    BookingModule,
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
