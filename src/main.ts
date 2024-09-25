efad19c9a0552880b509d78acfff63033d172625
Dreyan Franco <dreyanfranco@gmail.com>
2024-09-25 16:13:25 +0200

    preparing for deployment

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 5002;
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +12,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
